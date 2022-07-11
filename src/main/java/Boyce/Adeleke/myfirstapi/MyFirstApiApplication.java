0eb1da6f9a331431e6a8df1cdd5c9bf3c07dd806
Boyce007 <diboyce21@students.desu.edu>
2022-07-11 14:01:12 -0400

    Updates

---
+++ src/main/java/Boyce/Adeleke/myfirstapi/MyFirstApiApplication.java
@@ -0,0 +1,13 @@
+package Boyce.Adeleke.myfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstApiApplication.class, args);
+	}
+
+}
